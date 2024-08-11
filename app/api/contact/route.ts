f57dfdb27aee876bd1314856c59e2737e99562cf
Rhys Campbell <rhysmalcolm13@outlook.com>
2024-08-11 12:55:42 +1000

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,43 +1,37 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-// import type { NextRequest } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-export const POST = async (req: Request) => {
-  const { name, email, message, phone } = await req.json();
-  console.log(name, email, message, phone);
-
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASS,
-    },
-  });
-
-  const mailOptions = {
-    from: email,
-    to: process.env.USER,
-    subject: `Website Submission from ${name}`,
-    replyTo: email,
-    html: `
-      <p>Name:</p>
-      <p>  ${name} </p>
-      <p>Message:</p>
-      <p>  ${message} </p>
-      <p>Email:</p>
-      <p>  ${email} </p>
-      <p>Phone:</p>
-      <a href="tel:${phone}">${phone}</a>
-    `,
-    text: `${message} Send from ${email}`,
-  };
-
+export const POST = async (req: NextRequest) => {
   try {
+    const { student, studentEmail, complaintSubject, mainComplaint } = await req.json();
+    
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: studentEmail,
+      to: process.env.USER,
+      subject: `Complaint Submission: ${complaintSubject}`,
+      replyTo: studentEmail,
+      html: `
+        <p><strong>Student Name:</strong> ${student}</p>
+        <p><strong>Email:</strong> ${studentEmail}</p>
+        <p><strong>Subject:</strong> ${complaintSubject}</p>
+        <p><strong>Complaint:</strong> ${mainComplaint}</p>
+      `,
+      text: `${mainComplaint} (Sent from ${studentEmail})`,
+    };
+
     await transporter.sendMail(mailOptions);
     return NextResponse.json({ status: "Ok" });
+
   } catch (error) {
-    console.log(error);
+    console.error("Error sending email:", error);
     return NextResponse.json({ error: "Error sending email" }, { status: 500 });
   }
 };
